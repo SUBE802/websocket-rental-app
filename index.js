8304607402d305779f1e3e4a8126c3073b374944
SUBE802 <tandonsubekshya88@gmail.com>
2024-04-01 14:46:12 +0545

    modified chat code.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const keys = {
     key: fs.readFileSync('./cert/localhost.key'),
     cert: fs.readFileSync('./cert/localhost.crt')
-  };
+};
 
 app.use(cors());
 const server = https.createServer(keys, app);
@@ -17,41 +17,54 @@ const server = https.createServer(keys, app);
 
 const io = new socketIO.Server(server,{
     cors: {
-        origin: "https://apex.oracle.com",  
+        origin: ["https://apex.oracle.com"],  
         methods: ["GET", "POST"],
         credentials: true
       },
       allowEIO3: true 
 });
-const activeUsers = new Set();
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World');
-// })
-
+const activeUsers = [];
 
 io.on('connection', (socket) => {
     console.log('A user connected');
+    socket.on('user-connected', (data) => {
+        console.log('New User Connected', data);
+        activeUsers.push({
+            name: data.name,
+            socketId : socket.id,
+            userId: data.userId
+        });
+    })
+
+    socket.on('send-message', (data) => {
+        console.log('new message received', data);
+
+        const receiverSocket = activeUsers.find((user) => user.userId === data.receiver);
+        if (receiverSocket !== undefined) {
+            socket.to(receiverSocket.socketId).emit('recieve-message', {
+                senderId : data.senderId,
+                message : data.message,
+                date: data.date
+            });
+        }
+    })
 
     socket.on('req-refresh-chat', (data) => {
         console.log('asked to refresh user chat', data);
         socket.emit('refresh-chat');
     })
 
-    socket.on('user-connected', (data) => {
-        // users[socket.id] = name;
-        socket.userId = data;
-        activeUsers.add(data);
-    });
-
-    socket.on('message', (data) => {
-        console.log(`Received message: ${data}`);
-        io.emit('message', data);
-    });
     socket.on('disconnect', () => {
-        activeUsers.delete(socket.userId);
+        const disconnectedUserID = activeUsers.findIndex((user) => user.socketId === socket.id);
+        activeUsers.splice(disconnectedUserID,1);
         console.log('A user disconnected');
     });
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
 server.listen(3000, () => {
